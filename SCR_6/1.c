9f08d241a6990f47aaf2c7f0a41b92964ce291ad
adriansobecki <adriansobecki@onet.pl>
2020-12-10 10:18:40 +0100

    6

---
+++ SCR_6/1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+int main()
+{
+  char n_pliku[50];
+  int plik_map;
+  int fd;
+  FILE* fd_r;
+  size_t rozmiar;
+  char* obszar_pamieci;
+  struct stat buf;
+  plik_map = open("plik_map", O_RDWR | O_CREAT);
+  if(plik_map==-1)
+    {
+    printf(" %s\n", strerror(errno));
+    return -1;
+    }
+  
+  while(1)
+ {
+      printf("Podaj nazwe pliku\n");
+      scanf("%s",n_pliku);
+      if(n_pliku[0]=='.')
+	break;
+      fd=open(n_pliku,O_RDONLY);
+      fstat(fd, &buf);
+
+      //printf("%ld\n",buf.st_size);
+      
+      ftruncate(plik_map, buf.st_size);
+      
+      //fstat(plik_map, &buf);
+      //printf("%ld\n",buf.st_size);
+	
+      obszar_pamieci=mmap(NULL, buf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, plik_map, 0);
+      if (obszar_pamieci == MAP_FAILED)
+	printf("blad");
+      printf(" %s\n", strerror(errno));
+
+      int i=0;
+      close(fd);
+      fd_r=fopen(n_pliku,"r");
+      
+      while(!feof(fd_r))
+	{
+	  obszar_pamieci[i++]=getc(fd_r);
+   	}
+      fclose(fd_r);
+      //msync(obszar_pamieci, buf.st_size, MS_SYNC);
+      munmap(obszar_pamieci, buf.st_size);
+ }
+  close(plik_map);
+}
